26deb77ac68aaf2b6f8219fb4243b1c0e32f34e6
Makcik303123 <adoberoflit@gmail.com>
2024-04-11 00:31:51 +0300

    continue header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,36 @@
 import React from "react";
 import styles from "./Header.module.scss";
 import logo from "../../assets/Header/logo.svg";
+import { Button, ButtonType } from "../../ui/button";
 
 const navLinks = ["Home", "Category", "About", "Contact"];
 
 export const Header = () => {
   return (
     <div className={styles.header}>
-      <img width={115} height={40} src={logo} alt="logo" />
-      <div className={styles.nav}>
-        <ul className={styles.list}>
-          <li>Home</li>
-          <li>Home</li>
-          <li>Home</li>
-          <li>Home</li>
-        </ul>
-      </div>
+      <>
+        <img
+          className={styles.logo}
+          width={115}
+          height={40}
+          src={logo}
+          alt="logo"
+        />
+        <div className={styles.nav}>
+          <ul className={styles.list}>
+            {navLinks.map((link) => (
+              <li className={styles.item}>{link}</li>
+            ))}
+          </ul>
+        </div>
+      </>
+      <>
+        <div>Search</div>
+        <Button type={ButtonType.DEFOLT}>
+          <span>Join the community</span>
+        </Button>
+        <div>Dropdown</div>
+      </>
     </div>
   );
 };
