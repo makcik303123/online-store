dcb5b7768e57eb60171729c84f8fe2e6b9fdadc4
Makcik303123 <adoberoflit@gmail.com>
2024-04-11 23:57:00 +0300

    finish header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,36 +2,36 @@ import React from "react";
 import styles from "./Header.module.scss";
 import logo from "../../assets/Header/logo.svg";
 import { Button, ButtonType } from "../../ui/button";
-import { Input } from "../../ui/input";
+import { Search } from "../Search";
 
 const navLinks = ["Home", "Category", "About", "Contact"];
 
 export const Header = () => {
-	return (
-		<div className={styles.header}>
-			<>
-				<img
-					className={styles.logo}
-					width={115}
-					height={40}
-					src={logo}
-					alt="logo"
-				/>
-				<div className={styles.nav}>
-					<ul className={styles.list}>
-						{navLinks.map((link) => (
-							<li className={styles.item}>{link}</li>
-						))}
-					</ul>
-				</div>
-			</>
-			<>
-				<Input placeHolder={"Search"} />
-				<Button type={ButtonType.DEFOLT}>
-					<span>Join the community</span>
-				</Button>
-				<div>Dropdown</div>
-			</>
-		</div>
-	);
+  return (
+    <div className={styles.header}>
+      <div className={styles.nav}>
+        <img
+          className={styles.logo}
+          width={115}
+          height={40}
+          src={logo}
+          alt="logo"
+        />
+
+        <ul className={styles.list}>
+          {navLinks.map((link) => (
+            <li className={styles.item}>{link}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div className={styles.aside}>
+        <Search placeHolder={"Search"} />
+        <Button type={ButtonType.DEFOLT}>
+          <span>Join the community</span>
+        </Button>
+        <div>VND ^</div>
+      </div>
+    </div>
+  );
 };
