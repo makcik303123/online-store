b9bd0b28602266e3a16b6fd16ecd204c796b1e61
dmitry <ddovidovich@list.ru>
2024-04-13 16:44:19 +0300

    create component-card

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
-import { Header } from "./components/Header/Header";
-import { Button, ButtonType } from "./ui/button";
+// import {Header} from "./components/Header/Header";
+// import {Button, ButtonType} from "./ui/button";
+import CardContainer from "./ui/cardContainer/CardContainer.tsx";
+import { ourProduct } from "./ui/cardContainer/cardsProducts.data.ts";
+import { ourDogs } from "./ui/cardContainer/cardsDogs.data.ts";
+
+export type StyleType = "products" | "dogs";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div>
-        <Button type={ButtonType.DEFOLT}>
-          <span>Сommunity</span>
-          <span> +</span>
-        </Button>
+      {/*<Header />*/}
+      {/*<div>*/}
+      {/*  <Button type={ButtonType.DEFOLT}>*/}
+      {/*    <span>Сommunity</span>*/}
+      {/*    <span> +</span>*/}
+      {/*  </Button>*/}
 
-        <Button type={ButtonType.EMPTY}>
-          <span>Explore Now</span>
-        </Button>
-        <Button type={ButtonType.WHITE}>
-          <span>Explore Now</span>
-        </Button>
-        <Button type={ButtonType.WHITEBORDER}>
-          <span>Explore Now</span>
-        </Button>
-      </div>
+      {/*  <Button type={ButtonType.EMPTY}>*/}
+      {/*    <span>Explore Now</span>*/}
+      {/*  </Button>*/}
+      {/*  <Button type={ButtonType.WHITE}>*/}
+      {/*    <span>Explore Now</span>*/}
+      {/*  </Button>*/}
+      {/*  <Button type={ButtonType.WHITEBORDER}>*/}
+      {/*    <span>Explore Now</span>*/}
+      {/*  </Button>*/}
+      {/*</div>*/}
+
+      {/* <CardContainer cards={ourProduct} style='products'/> */}
+      <CardContainer cards={ourDogs} style="dogs" />
     </div>
   );
-
 }
 
 export default App;
---
+++ src/ui/cardInfo/CardInfo.tsx
@@ -0,0 +1,28 @@
+import {productInfoType} from "../cardContainer/cardsProducts.data.ts";
+import styles from './cardInfo.module.scss'
+import {dogInfoType} from "../cardContainer/cardsDogs.data.ts";
+
+interface CardInfoProps {
+    info: productInfoType | dogInfoType
+}
+
+export const CardInfo = (props: CardInfoProps) => {
+
+    const cardInfo = Object.entries(props.info)
+
+    return (
+        <div className={styles.cardInfoContainer}>
+            {cardInfo.map((p, index) => {
+                const classDot = index % 2 === 1 ? styles.dot : ''
+                const [name, value] = p
+
+                return (<p key={index} className={classDot}>
+                        <span className={styles.name}>{name}: </span><span className={styles.value}>{value}</span>
+                    </p>
+                )
+            })}
+        </div>
+    )
+        ;
+};
+
