3f72de28372efbf1ef424b3b10ef4a09b3364b20
Makcik303123 <adoberoflit@gmail.com>
2024-04-18 23:52:28 +0300

    create checkBox

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,5 +1,6 @@
 import { SectionHeading } from "./sectionHeading";
 import { Button } from "./button";
 import { Heading } from "./typography";
+import { CheckBox } from "./checkBox";
 
-export { SectionHeading, Button, Heading };
+export { SectionHeading, Button, Heading, CheckBox };
