d013f0e98801eac6846a1623de701469e8bfa8e0
Maksim <adoberoflit@gmail.com>
2024-04-22 13:57:44 +0300

    npm install

--- src/main.tsx
+++ src/main.tsx
@@ -1,45 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
+import "reflect-metadata";
+import {
+	createBrowserRouter,
+	Navigate,
+	RouterProvider,
+} from "react-router-dom";
 import App from "./App.tsx";
 import "./index.scss";
 import "./libs/reset.scss";
 import "./libs/vars.scss";
-import {Category, Dogs, Home} from "./pages/index.ts";
+import { Category, Dogs, Home } from "./pages/index.ts";
 
 const PATH = {
-    HOME: '/home',
-    CATEGORY: '/category',
-    DOG: '/dogs'
-} as const
+	HOME: "/home",
+	CATEGORY: "/category",
+	DOG: "/dogs",
+} as const;
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Navigate to={PATH.HOME}/>
-    },
-    {
-        element: <App/>,
-        errorElement: <span style={{color: "black"}}>Error</span>,
-        children: [
-            {
-                path: PATH.HOME,
-                element: <Home/>,
-            },
-            {
-                path: PATH.CATEGORY,
-                element: <Category/>,
-            },
-            {
-                path: PATH.DOG,
-                element: <Dogs/>,
-            },
-        ],
-    },
+	{
+		path: "/",
+		element: <Navigate to={PATH.HOME} />,
+	},
+	{
+		element: <App />,
+		errorElement: <span style={{ color: "black" }}>Error</span>,
+		children: [
+			{
+				path: PATH.HOME,
+				element: <Home />,
+			},
+			{
+				path: PATH.CATEGORY,
+				element: <Category />,
+			},
+			{
+				path: PATH.DOG,
+				element: <Dogs />,
+			},
+		],
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <RouterProvider router={router}/>
-    </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
