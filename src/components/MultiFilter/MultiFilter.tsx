ff350ad184683f9920fe6cc2f3d10e596da97cd4
Makcim <adoberoflit@gmail.com>
2024-05-16 11:06:12 +0300

    deploy vercel

--- src/components/MultiFilter/MultiFilter.tsx
+++ src/components/MultiFilter/MultiFilter.tsx
@@ -5,34 +5,35 @@ import { useMultiFilterState } from "./useMultiFilterState";
 import type { MultiFilterProps } from "./types";
 
 export const MultiFilter = ({
-  openFilter,
-  setOpenFilter,
+	openFilter,
+	setOpenFilter,
 }: MultiFilterProps) => {
-  const { params, handlerCheckBox } = useMultiFilterState();
+	const { params, handlerCheckBox } = useMultiFilterState();
+	console.log(params);
 
-  const className =
-    styles.multifilter__wrapper + (openFilter ? ` ${styles.active}` : "");
+	const className =
+		styles.multifilter__wrapper + (openFilter ? ` ${styles.active}` : "");
 
-  return (
-    <div className={className} onClick={() => setOpenFilter(false)}>
-      <div className={styles.multifilter} onClick={(e) => e.stopPropagation()}>
-        <div className={styles.heading__wrapper}>
-          <Heading>Filter</Heading>
-        </div>
-        {filters.map((filter, i) => (
-          <div key={i} className={styles.filter}>
-            <div className={styles.filter__header}>{filter.header}</div>
-            {filter.fields.map((field) => (
-              <CheckBox
-                key={field.name}
-                label={field.name}
-                color={field.color}
-                handler={handlerCheckBox}
-              />
-            ))}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className={className} onClick={() => setOpenFilter(false)}>
+			<div className={styles.multifilter} onClick={(e) => e.stopPropagation()}>
+				<div className={styles.heading__wrapper}>
+					<Heading>Filter</Heading>
+				</div>
+				{filters.map((filter, i) => (
+					<div key={i} className={styles.filter}>
+						<div className={styles.filter__header}>{filter.header}</div>
+						{filter.fields.map((field) => (
+							<CheckBox
+								key={field.name}
+								label={field.name}
+								color={field.color}
+								handler={handlerCheckBox}
+							/>
+						))}
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
