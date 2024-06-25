5a82bf5b814ac2ab1d9d52c07010d7d4499f8b4a
Makcim <adoberoflit@gmail.com>
2024-06-25 17:56:37 +0300

    add mapchart

--- src/pages/About/MapChart.jsx
+++ src/pages/About/MapChart.jsx
@@ -14,8 +14,8 @@ const markers = [
 		name: "Buenos Aires",
 		coordinates: [-58.3816, -34.6037],
 	},
-	{ markerOffset: 15, name: "La Paz", coordinates: [-68.1193, -16.4897] },
-	{ markerOffset: 15, name: "Brasilia", coordinates: [-47.8825, -15.7942] },
+	{ markerOffset: 15, name: "Bolivia", coordinates: [-68.1193, -16.4897] },
+	{ markerOffset: 15, name: "Brazil", coordinates: [-47.8825, -15.7942] },
 	{ markerOffset: 15, name: "Santiago", coordinates: [-70.6693, -33.4489] },
 	{ markerOffset: 15, name: "Bogota", coordinates: [-74.0721, 4.711] },
 	{ markerOffset: 15, name: "Quito", coordinates: [-78.4678, -0.1807] },
@@ -32,6 +32,12 @@ console.log(Geographies, " Geographies");
 console.log(Geography, " Geography");
 
 const MapChart = () => {
+	const [activeName, setActiveName] = React.useState(
+		markers.map((marker) => marker.name)
+	);
+
+	console.log(activeName);
+
 	return (
 		<ComposableMap
 			// projection="geoAzimuthalEqualArea"
@@ -43,55 +49,70 @@ const MapChart = () => {
 			<Geographies geography={"./features.json"}>
 				{({ geographies }) =>
 					geographies.map((geo) => {
-						// console.log(geographies);
 						return (
 							<Geography
 								key={geo.rsmKey}
 								geography={geo}
 								fill="#EAEAEC"
 								stroke="#D6D6DA"
-								// style={{
-								//   default: {
-								//     fill: "#EEE",
-								//     transition: "fill 0.5s",
-								//     outline: "none !important",
-								//   },
-								//   hover: {
-								//     fill: "#F53",
-								//     transition: "fill 0.5s",
-								//     outline: "none",
-								//   },
-								//   pressed: {
-								//     fill: "#E42",
-								//     border: "none",
-								//     outline: "none",
-								//   },
-								// }}
+								style={{
+									default: {
+										fill: "#EEE",
+										transition: "fill 0.5s",
+										outline: "none !important",
+										position: "relative",
+										zIndex: 30,
+									},
+									hover: {
+										fill: "#003459",
+										transition: "fill 0.5s",
+										outline: "none",
+									},
+									pressed: {
+										fill: "#001e34",
+										border: "none",
+										outline: "none",
+									},
+								}}
 							/>
 						);
 					})
 				}
 			</Geographies>
 			{markers.map(({ name, coordinates, markerOffset }) => (
-				<Marker key={name} coordinates={coordinates}>
-					<g
-						fill="none"
-						stroke="#FF5533"
-						strokeWidth="2"
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						transform="translate(-12, -24)"
-					>
-						<circle cx="12" cy="10" r="3" />
-						<path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
-					</g>
-					{/* <text
-            textAnchor="middle"
-            y={markerOffset}
-            style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
-          >
-            {name}
-          </text> */}
+				<Marker
+					key={name}
+					coordinates={coordinates}
+					onMouseEnter={() => setActiveName(name)}
+				>
+					{activeName === name ? (
+						<text
+							textAnchor="middle"
+							y={markerOffset}
+							style={{
+								fontFamily: "system-ui",
+								fill: "#5D5A6D",
+								// opacity: activeName === name ? 1 : 0,
+								transition: "opacity 0.5s",
+								position: "relative",
+								zIndex: 1,
+							}}
+						>
+							{name}
+						</text>
+					) : (
+						<g
+							fill="none"
+							stroke="#FF5533"
+							strokeWidth="2"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							transform="translate(-12, -24)"
+						>
+							<circle cx="12" cy="10" r="3" />
+							<path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+						</g>
+					)}
 				</Marker>
 			))}
 		</ComposableMap>
